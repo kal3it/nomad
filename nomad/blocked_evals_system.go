d80ebe72fc4af0c9b78aa4782e5f9585d7a89b4a
Lang Martin <lang@hashicorp.com>
2019-07-02 10:21:12 -0400

    blocked_evals_system cleanup interface, make the collections clean

--- nomad/blocked_evals_system.go
+++ nomad/blocked_evals_system.go
@@ -4,71 +4,91 @@ import "github.com/hashicorp/nomad/nomad/structs"
 
 // systemEvals are handled specially, each job may have a blocked eval on each node
 type systemEvals struct {
-	// job maps a single blocked eval on each node for each job
-	job map[structs.NamespacedID]map[string]*structs.Evaluation
+	// byJob maps a jobID to a nodeID to that job's single blocked evalID on that node
+	byJob map[structs.NamespacedID]map[string]string
 
-	// node maps a node id to the set of all blocked evals, mapped to their authentication tokens
-	// node map[string]map[*structs.Evaluation]string
-	node map[string]map[string]*wrappedEval
+	// byNode maps a nodeID to a set of evalIDs
+	byNode map[string]map[string]bool
+
+	// evals maps evalIDs to an eval and token
+	evals map[string]*wrappedEval
 }
 
 func newSystemEvals() *systemEvals {
 	return &systemEvals{
-		job: map[structs.NamespacedID]map[string]*structs.Evaluation{},
-		// node: map[string]map[*structs.Evaluation]string{},
-		node: map[string]map[string]*wrappedEval{},
+		evals:  map[string]*wrappedEval{},
+		byJob:  map[structs.NamespacedID]map[string]string{},
+		byNode: map[string]map[string]bool{},
 	}
 }
 
-// setSystemEval creates the inner map if necessary
-func (b *BlockedEvals) setSystemEval(eval *structs.Evaluation, token string) {
+func (s *systemEvals) Add(eval *structs.Evaluation, token string) {
 	// store the eval by node id
-	if _, ok := b.system.node[eval.NodeID]; !ok {
-		// b.system.node[eval.NodeID] = make(map[*structs.Evaluation]string)
-		b.system.node[eval.NodeID] = make(map[string]*wrappedEval)
+	if _, ok := s.byNode[eval.NodeID]; !ok {
+		s.byNode[eval.NodeID] = make(map[string]bool)
 	}
-	b.system.node[eval.NodeID][eval.ID] = &wrappedEval{eval: eval, token: token}
+
+	s.byNode[eval.NodeID][eval.ID] = true
+	s.evals[eval.ID] = &wrappedEval{eval: eval, token: token}
 
 	// link the job to the node for cleanup
 	jobID := structs.NewNamespacedID(eval.JobID, eval.Namespace)
-	if _, ok := b.system.job[jobID]; !ok {
-		b.system.job[jobID] = make(map[string]*structs.Evaluation)
+	if _, ok := s.byJob[jobID]; !ok {
+		s.byJob[jobID] = make(map[string]string)
 	}
 
-	// if we're displacing the old blocked id for this node, clean delete it first
-	if prev, ok := b.system.job[jobID][eval.NodeID]; ok {
-		b.delSystemEval(prev)
+	// if we're displacing the old blocked id for this job+node, delete it first
+	if prevID, ok := s.byJob[jobID][eval.NodeID]; ok {
+		prev, _ := s.Get(prevID)
+		s.Del(prev.eval)
 	}
 
 	// set this eval as the new eval for this job on this node
-	b.system.job[jobID][eval.NodeID] = eval
+	s.byJob[jobID][eval.NodeID] = eval.ID
 }
 
-// delSystemEval deletes a blocked system eval
-func (b *BlockedEvals) delSystemEval(eval *structs.Evaluation) {
-	// delete the job index iff this eval is the currently listed blocked eval
+func (s *systemEvals) Get(evalID string) (*wrappedEval, bool) {
+	w, ok := s.evals[evalID]
+	return w, ok
+}
+
+func (s *systemEvals) Del(eval *structs.Evaluation) {
+	// delete the job index if this eval is the currently listed blocked eval
 	jobID := structs.NewNamespacedID(eval.JobID, eval.Namespace)
-	e, ok := b.system.job[jobID][eval.NodeID]
-	if ok && e.ID == eval.ID {
-		delete(b.system.job[jobID], eval.NodeID)
+	e, ok := s.byJob[jobID][eval.NodeID]
+	if ok && e == eval.ID {
+		delete(s.byJob[jobID], eval.NodeID)
 	}
 
-	// if we're deleting from system.node, decrement stats
-	if _, ok = b.system.node[eval.NodeID][eval.ID]; ok {
-		b.stats.TotalBlocked--
-		if len(b.system.node[eval.NodeID]) == 1 {
-			delete(b.system.node, eval.NodeID)
-		} else {
-			delete(b.system.node[eval.NodeID], eval.ID)
+	// delete this eval from the node index, and then the map for this node if empty
+	delete(s.byNode[eval.NodeID], eval.ID)
+	if len(s.byNode[eval.NodeID]) == 0 {
+		delete(s.byNode, eval.NodeID)
+	}
+
+	// delete the eval itself
+	delete(s.evals, eval.ID)
+}
+
+func (s *systemEvals) NodeEvals(nodeID string) (map[*structs.Evaluation]string, bool) {
+	out := map[*structs.Evaluation]string{}
+	for eID := range s.byNode[nodeID] {
+		if w, ok := s.Get(eID); ok {
+			out[w.eval] = w.token
 		}
 	}
+
+	ok := len(out) > 0
+	return out, ok
 }
 
-// getSystemEvals returns the set of blocked evals for namespaced job
-func (b *BlockedEvals) getSystemEvals(jobID structs.NamespacedID) []*structs.Evaluation {
-	var out []*structs.Evaluation
-	for _, e := range b.system.job[jobID] {
-		out = append(out, e)
+func (s *systemEvals) JobEvals(jobID structs.NamespacedID) ([]*structs.Evaluation, bool) {
+	out := []*structs.Evaluation{}
+	_, ok := s.byJob[jobID]
+	for _, eID := range s.byJob[jobID] {
+		if e, ok := s.Get(eID); ok {
+			out = append(out, e.eval)
+		}
 	}
-	return out
+	return out, ok
 }
